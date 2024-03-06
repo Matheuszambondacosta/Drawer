94bc86de32f3b13962a6c3157ec3c6e2c9fd34c3
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-06 10:55:44 -0300

    Adiciona telas Home, Contact e Profile e configura navegação por gaveta

--- App.jsx
+++ App.jsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { StatusBar } from 'react-native';
+
+
+import Home from './src/components/Title/index';
+import Contact from './src/screens/Contact/index';
+import Profile from './src/screens/Profile/index';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Contact" component={Contact} />
+        <Drawer.Screen name="Profile" component={Profile} />
+      
+
+      </Drawer.Navigator>
+
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
