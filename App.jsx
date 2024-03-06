89c5f29baea6462a29796a820015fd918abc85df
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-06 11:05:08 -0300

    Adiciona estilos aos componentes Contact, Home e Profile

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StatusBar } from 'react-native';
+import styles from './src/screens/Contact/styles';
 
 
 import Home from './src/components/Title/index';
@@ -17,7 +18,7 @@ export default function App() {
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Contact" component={Contact} />
         <Drawer.Screen name="Profile" component={Profile} />
-      
+
 
       </Drawer.Navigator>
 
